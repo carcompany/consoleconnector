300d9e4a7e2a82f751c0c70c59d6aa0b93e01dc7
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-11 17:51:40 +0200

    Added ConsoleWrapper class

---
+++ src/main/java/com/carcompany/consoleconnector/ConsoleWrapper.java
@@ -0,0 +1,47 @@
+package com.carcompany.consoleconnector;
+
+import java.util.Scanner;
+
+public class ConsoleWrapper {
+
+
+    private static ConsoleWrapper consoleWrapper;
+
+
+    private Scanner console;
+
+
+
+    public ConsoleWrapper(){
+
+        this.console = new Scanner(System.in);
+
+    }
+
+    public static ConsoleWrapper getInstance(){
+        if(consoleWrapper == null) consoleWrapper = new ConsoleWrapper();
+        return consoleWrapper;
+    }
+
+    public String getInput(){
+        System.out.print("Slotos@CarCompany ⛺️ % ");
+		return console.nextLine();
+    }
+
+    public String ask4Input(String question){
+
+        System.out.println(question);
+        return this.getInput();
+
+
+    }
+
+
+
+
+    
+}
+
+
+
+
