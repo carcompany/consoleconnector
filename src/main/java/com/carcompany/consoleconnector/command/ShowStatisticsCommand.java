ec4cc15168039541ffa92c522aab9b3e440fac49
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-11 16:22:35 +0200

    Added ShowStatistics Command and View

--- src/main/java/com/carcompany/consoleconnector/command/ShowStatisticsCommand.java
+++ src/main/java/com/carcompany/consoleconnector/command/ShowStatisticsCommand.java
@@ -1,5 +1,12 @@
 package com.carcompany.consoleconnector.command;
 
+import com.carcompany.carreservationservice.structure.bookingservice.structure.Language;
+import com.carcompany.carreservationservice.structure.statisticsservice.structure.ExternalPaymentServiceEnumeration;
+import com.carcompany.carreservationservice.structure.statisticsservice.structure.services.Statistic;
+import com.carcompany.consoleconnector.CarReservationServiceObservable;
+import com.carcompany.consoleconnector.view.ShowStatisticsView;
+import com.carcompany.consoleconnector.view.View;
+
 /**
  * @author Sebastian
  * @version 1.0
@@ -16,6 +23,14 @@ public class ShowStatisticsCommand extends Command {
 	 * @param arguments
 	 */
 	public void executeCommand(String[] arguments) {
+
+		if(arguments.length > 0 && arguments.length < 3){
+			Statistic statistic = CarReservationServiceObservable.getInstance().showStatistics(Language.valueOf(arguments[0]), ExternalPaymentServiceEnumeration.valueOf(arguments[1]));
+			
+			View view = new ShowStatisticsView(statistic);
+			view.print();
+
+		}
 
 	}
 }
