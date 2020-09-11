f039d1bd9ca93c086dac4ef93562f102cbf06ac3
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-11 16:49:54 +0200

    Implemented CreateResource Command and View

--- src/main/java/com/carcompany/consoleconnector/command/CreateResourceCommand.java
+++ src/main/java/com/carcompany/consoleconnector/command/CreateResourceCommand.java
@@ -1,5 +1,16 @@
 package com.carcompany.consoleconnector.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.carcompany.carreservationservice.structure.resourceservice.structure.Resource;
+import com.carcompany.carreservationservice.structure.resourceservice.structure.ResourceEnumeration;
+import com.carcompany.carreservationservice.structure.resourceservice.structure.exception.MoreThanOneDecoratableResourceException;
+import com.carcompany.carreservationservice.structure.resourceservice.structure.exception.NoDecoratableResourceException;
+import com.carcompany.consoleconnector.CarReservationServiceObservable;
+import com.carcompany.consoleconnector.view.CreateResourceView;
+import com.carcompany.consoleconnector.view.View;
+
 /**
  * @author Sebastian
  * @version 1.0
@@ -7,7 +18,29 @@ package com.carcompany.consoleconnector.command;
  */
 public class CreateResourceCommand extends Command {
 
-	public void executeCommand(String[] arguments) {
+	public void executeCommand(String[] arguments)
+			throws MoreThanOneDecoratableResourceException, NoDecoratableResourceException {
+		
+
+
+		List<ResourceEnumeration> list = new ArrayList<ResourceEnumeration>();
+
+
+		if( arguments.length > 0){
+			for (String string : arguments) {
+				list.add(ResourceEnumeration.valueOf(string));
+				
+			}
+		}
+		Resource resource = CarReservationServiceObservable.getInstance().createResource(list.toArray(new ResourceEnumeration[list.size()]));
+
+		View view = new CreateResourceView(resource);
+
+		view.print();
+		view.update();
+
+
+		
 
 	}
 }
