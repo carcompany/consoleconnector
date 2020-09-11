f5cdc976a115f19968140748d7ab9579eeab6ca2
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-11 18:47:50 +0200

    Added CreateBooking View and Command

--- src/main/java/com/carcompany/consoleconnector/view/CreateBookingView.java
+++ src/main/java/com/carcompany/consoleconnector/view/CreateBookingView.java
@@ -1,5 +1,8 @@
 package com.carcompany.consoleconnector.view;
 
+import com.carcompany.carreservationservice.structure.bookingservice.structure.Booking;
+import com.carcompany.carreservationservice.structure.personservice.structure.*;
+
 /**
  * @author Benjamin Esdor
  * @version 1.0
@@ -7,15 +10,36 @@ package com.carcompany.consoleconnector.view;
  */
 public class CreateBookingView extends View {
 
-	public CreateBookingView() {
+
+	private Booking booking;
+
+	public CreateBookingView(Booking booking) {
+
+		this.booking = booking;
 
 	}
 
 	public void print() {
 
+		System.out.println("–= BOOKING CREATED =–");
+		System.out.println("FOR:");
+
+		Person person = booking.getHead().getPerson();
+
+		if (person instanceof NaturalPerson) {
+			System.out.println("Firstname: " + ((NaturalPerson) person).getFirstname());
+			System.out.println("Lastname: " + ((NaturalPerson) person).getLastname());
+		} else {
+			System.out.println("Company name: " + ((LegalPerson) person).getName());
+		}
+		System.out.println("PRICE:");
+		System.out.println(booking.getBody().getResource().getPrice());
+
 	}
 
 	public void update() {
+
+		store(booking);
 
 	}
 }
